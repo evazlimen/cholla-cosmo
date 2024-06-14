218d61b22a85beb401c59408aa70d016210eeb3b
Brant Robertson <brantr@users.noreply.github.com>
2024-06-14 16:14:16 -0700

    Trying to get ride of lumache stuff

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -4,4 +4,4 @@ API
 .. autosummary::
    :toctree: generated
 
-   lumache
+   cholla-cosmo
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cholla-Cosmo'
+copyright = '2024, UCSC Comp Astro Research Group'
+author = 'UCSC Comp Astro Research Group'
 
 release = '0.1'
 version = '0.1.0'
